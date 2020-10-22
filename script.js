a6e65f355c8f07b25522cb7016081e62ad6c4394
فاطمه حسين شيشتر <basicfatma@users.noreply.github.com>
2020-10-22 21:37:11 +0300

    d

---
+++ script.js
@@ -0,0 +1,33 @@
+let imArr = document.querySelectorAll(".img");
+let prevX = 0;
+let prevY = 0;
+let moveXAmount = 0;
+let moveYAmount = 0;
+
+
+
+
+document.addEventListener("mousemove", function(e){
+ mousePos(e);
+   
+})
+
+function mousePos(e){
+    moveXAmount = e.pageX - prevX;
+    moveYAmount = e.pageY - prevY;
+
+    moveImg(moveXAmount, moveYAmount);
+
+
+    prevX = e.pageX;
+    prevY = e.pageY;
+}
+
+function   moveImg(xAmount, yAmount);
+
+
+igmArr.forEach(function(img){
+    let movementStrength = 5 + (math.random()  * 15);
+    img.style.left = (img.offsetleft) - (xAmount/movementStrength) + "px";
+    img.style.top = (img.offsetTop) - (yAmount/movementStrength) + "px" ;
+})
